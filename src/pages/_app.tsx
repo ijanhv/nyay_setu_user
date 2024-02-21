9f6fa453d63c481aa250b196fd20dee477fc6a86
Janhavi Patil <patil.janhavi1207@gmail.com>
2024-02-21 10:21:15 +0530

    feat: change url

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,10 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <ThemeProvider attribute="class" defaultTheme="light">
     <ThirdwebProvider clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID} 
     activeChain={"mumbai"}
-    authConfig={{
-      domain: process.env.DOMAIN || "",
-      authUrl: "/api/auth"
-    }}
+   
   >
   <Component {...pageProps} />
   </ThirdwebProvider>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { QR } from "@/components/QR";
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <QR url="http://localhost:3000/case/0-16efe393-5e4b-4260-b406-d33efd68378c" />
+      <QR url="https://nyay-setu-user-zef8.vercel.app/git adcase/0-16efe393-5e4b-4260-b406-d33efd68378c" />
     </div>
   );
 }
