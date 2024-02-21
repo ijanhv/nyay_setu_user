46d00dc3c88b28edf76bdd5c0e3261e41d3d6334
Janhavi Patil <patil.janhavi1207@gmail.com>
2024-02-21 10:20:02 +0530

    feat: add theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import "@/styles/globals.css";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <ThemeProvider attribute="class" defaultTheme="light">
     <ThirdwebProvider clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID} 
     activeChain={"mumbai"}
     authConfig={{
@@ -13,5 +15,6 @@ export default function App({ Component, pageProps }: AppProps) {
   >
   <Component {...pageProps} />
   </ThirdwebProvider>
+  </ThemeProvider>
   );
 }
