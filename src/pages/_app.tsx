d198fd00638bed764c278567316ab37ec0b81282
Janhavi Patil <patil.janhavi1207@gmail.com>
2024-02-21 10:41:54 +0530

    feat: add shadcn

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,12 +6,16 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" defaultTheme="light">
-    <ThirdwebProvider clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID} 
-    activeChain={"mumbai"}
-   
-  >
-  <Component {...pageProps} />
-  </ThirdwebProvider>
-  </ThemeProvider>
+      <ThirdwebProvider
+        clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}
+        activeChain={"mumbai"}
+        authConfig={{
+          domain: process.env.DOMAIN || "",
+          authUrl: "/api/auth",
+        }}
+      >
+        <Component {...pageProps} />
+      </ThirdwebProvider>
+    </ThemeProvider>
   );
 }
--- src/pages/case/[caseId]/index.tsx
+++ src/pages/case/[caseId]/index.tsx
@@ -1,57 +1,57 @@
 import { useRouter } from 'next/router'
 import { useContract, useNFT } from '@thirdweb-dev/react'
 import Image from 'next/image'
+
+
 import Loader from '@/components/Loader'
 import ProgressTracker from '@/components/ProgressTracker'
 
-
-const Heading = ({ title }: { title: string}) => {
+const Heading = ({ title }: { title: string }) => {
   return (
     <div className="flex items-center gap-2 my-3">
-    <div className='bg-primary h-7 w-2 rounded-lg' />
-    <h4 className='text-xl font-bold '>{title}</h4>
+      <div className='bg-primary h-7 w-2 rounded-lg' />
+      <h4 className='text-xl font-bold '>{title}</h4>
     </div>
   )
 
-  }
+}
 
 const CaseDetailsPage = () => {
   const router = useRouter()
   const { caseId } = router.query
   const token = caseId?.toString().split('-')[0]
 
-  const { contract: caseCollection } = useContract("0xef26fB5eC61c546d2A6e05662FeB696ea3a01171")
+  const { contract: caseCollection } = useContract("0xEb542C9896Edd371788b5A12a92405cF09721849")
   const { data: nft, isLoading, error } = useNFT(caseCollection, token)
   // @ts-ignore
   const caseDetails = nft?.metadata?.properties
 
-  if(isLoading) return <Loader />
+  if (isLoading) return <Loader />
 
 
   return (
-    <div className='flex flex-col gap-3 m'>
+    <div className='p-10 flex flex-col gap-4 '>
       <div className='flex justify-between '>
-        <h1 className='heading'><span>Case Details</span></h1>
-        
+        <h1 className='font-semibold text-3xl'><span>Case Details</span></h1>
       </div>
       <div className='grid grid-cols-1 lg:grid-cols-2 gap-x-10 gap-y-6 my-5'>
         <div className='space-y-6'>
- 
-            <div className='relative h-[400px] w-full rounded-md  '>
-              <Image src={'/case.png'} alt='Case Image' fill className='object-cover rounded-lg' />
-            </div>
-            
-            <Heading title='Case Description' />
-         
+
+          <div className='relative h-[400px] w-full rounded-md  '>
+            <Image src={'/case.png'} alt='Case Image' fill className='object-cover rounded-lg' />
+          </div>
+
+          <Heading title='Case Description' />
+
+          {/* @ts-ignore */}
+          <p className='text-sm md:text-base text-justify'>{caseDetails?.caseDescription}</p>
+          <div className=' mt-5 flex justify-between'>
             {/* @ts-ignore */}
-            <p className='text-sm md:text-base text-justify'>{caseDetails?.caseDescription}</p>
-            <div className=' mt-5 flex justify-between'>
-              {/* @ts-ignore */}
-              <p>{caseDetails?.createdAt}</p>
-              {/* @ts-ignore */}
-              <p>Status:&nbsp;<span className='font-bold'>{caseDetails?.status}</span></p>
-            </div>
-        
+            <p>{caseDetails?.createdAt}</p>
+            {/* @ts-ignore */}
+            <p>Status:&nbsp;<span className='font-bold'>{caseDetails?.status}</span></p>
+          </div>
+
         </div>
         <div className=''>
           <div className='space-y-4'>
@@ -61,10 +61,10 @@ const CaseDetailsPage = () => {
               <Heading title='Case Details' />
               <div>
 
-                  
+
                 <h4 className='text-lg font-semibold underline'> Plaintiff Details</h4>
-      
-             
+
+
                 <div className='gap-y-2 grid grid-cols-1 lg:grid-cols-2 py-2'>
                   {/* @ts-ignore */}
                   <p><span className='font-bold'>Name:</span>&nbsp;{caseDetails?.plaintiff?.name}</p>
