1f2aaa13dbafe35215da4dc975d3cf14d64cc25b
Janhavi Patil <patil.janhavi1207@gmail.com>
2024-02-21 11:45:31 +0530

    feat: make url dynamic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import { QR } from "@/components/QR";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+  const { token } = router.query;
+
   return (
     <div className="flex items-center justify-center h-screen">
-      <QR url="https://nyay-setu-user.vercel.app/case/0-16efe393-5e4b-4260-b406-d33efd68378c" />
+      <QR
+        url={`https://nyay-setu-user.vercel.app/case/${token}-16efe393-5e4b-4260-b406-d33efd68378c`}
+      />
     </div>
   );
 }
